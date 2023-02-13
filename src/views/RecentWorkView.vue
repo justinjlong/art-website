68529459e3a20e6df6a0dbf3280ac13edb05e15f
Justin Long <jjl90@pitt.edu>
2023-02-12 20:48:09 -0500

    added bootstrap and recent works tab

---
+++ src/views/RecentWorkView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img src="@/assets/art/104k_buck.jpg" width="700">
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 90vh;
+    display: flex;
+    flex-direction: column !important;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
